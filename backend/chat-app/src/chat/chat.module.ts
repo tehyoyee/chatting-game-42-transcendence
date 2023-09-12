ecb10720f7e798d58f84ed54ee011f71120cdb30
지원 <jiwon@jiwon-ui-MacBookAir.local>
2023-09-12 16:55:29 +0900

    230912_1655pm

--- backend/chat-app/src/chat/chat.module.ts
+++ backend/chat-app/src/chat/chat.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Channel } from './entity/channel.entity';
+import { Message } from './entity/message.entity';
+import { UserChannelBridge } from './entity/user-channel-bridge.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Channel, Message, UserChannelBridge])],
   providers: [ChatService, ChatGateway],
   controllers: [ChatController]
 })
