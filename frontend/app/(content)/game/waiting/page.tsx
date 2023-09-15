c29a2ecfdc25173f247e0c8a74a240483b49faf6
jshin <arpegios@naver.com>
2023-09-15 16:48:12 +0900

    [UPDATE] waiting page

--- frontend/app/(content)/game/waiting/page.tsx
+++ frontend/app/(content)/game/waiting/page.tsx
@@ -5,27 +5,27 @@ import d from '/frontend/public/default.png'
 import { useAuthContext } from '@/components/user/auth';
 import { useEffect, useState } from 'react';
 export default function Page() {
-  const [match, setMatch] = useState(false);
+  // const [match, setMatch] = useState(false);
 
-  useEffect(() => {
-    (async() => {
-      fetch('http://localhost:3000/game/math', {
-        method: 'POST',
-        credentials: 'include',
-      })
-      .then(res => res.json())
-      .then(data => {
-          setMatch(true);
-      })
-      .catch(err => {
+  // useEffect(() => {
+  //   (async() => {
+  //     fetch('http://localhost:3000/game/math', {
+  //       method: 'POST',
+  //       credentials: 'include',
+  //     })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //         setMatch(true);
+  //     })
+  //     .catch(err => {
 
-      })
-    })()
-  })
+  //     })
+  //   })()
+  // })
 
   return (
   <>
-    <div className="centerItemBlock gridRow1_2 gridCol1_2">
+    {/* <div className="centerItemBlock gridRow1_2 gridCol1_2">
         <Image
           className={`${styles.profileImage}`}
           src={'/default.png'}
@@ -41,7 +41,7 @@ export default function Page() {
         height={128}
         width={128}
         alt={"profile image"} />
-    </div>}
+    </div>} */}
 
   </>
   );
