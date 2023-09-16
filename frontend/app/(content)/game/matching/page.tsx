f2151e369ca0e73d92a12926262a4060c940f66d
jshin <arpegios@naver.com>
2023-09-16 20:59:06 +0900

    [UPDATE] matching page

--- frontend/app/(content)/game/matching/page.tsx
+++ frontend/app/(content)/game/matching/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import d from '/frontend/public/default.png'
 import { useAuthContext } from '@/components/user/auth';
 import { useEffect, useState } from 'react';
+import Matching from '@/components/content/matching';
 export default function Page() {
   // const [match, setMatch] = useState(false);
 
@@ -25,7 +26,7 @@ export default function Page() {
 
   return (
   <>
-    matching room
+    <Matching></Matching>
   </>
   );
 }
---
+++ frontend/components/content/matching.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import defaultImage from '../../public/default.png';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import styles from '@/styles/matching.module.css';
+
+export default function Matching() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [ready, setReady] = useState(false);
+  const [countdown, setCountdown] = useState(3);
+
+  const handleReady = () => {
+    setReady(true);
+  };
+
+  useEffect(() => {
+    if (ready) {
+      const countdownInterval = setInterval(() => {
+        if (countdown <= 1) {
+          clearInterval(countdownInterval);
+          router.push('https://www.naver.com');
+        } else {
+          setCountdown(countdown - 1);
+        }
+      }, 1000);
+    }
+  }, [ready, countdown, router]);
+
+  return (
+    <div>
+      <div className={styles.userProfileContainer}>
+        <div className={styles.userProfile}>
+          <Image src={defaultImage} alt='profile image' className={styles.userImage} />
+          <div>user1</div>
+        </div>
+        <div className={styles.userProfile}>
+          <Image src={defaultImage} alt='profile image' className={styles.userImage} />
+          <div>user2</div>
+        </div>
+      </div>
+      {loading && <p>Loading...</p>}
+      {!loading && !ready && (
+        <div className={styles.buttonContainer}>
+          <button className={styles.button} onClick={() => setReady(true)}>Ready</button>
+        </div>
+      )}
+      {countdown !== null && ready && countdown > 0 && (
+        <div className={styles.countdownContainer}>
+          <p className={styles.countdown}>{countdown}</p>
+        </div>
+      )}
+      <div className={styles.bottomRight}>
+        <Link href='/game'>나가기</Link>
+      </div>
+    </div>
+  );
+}
