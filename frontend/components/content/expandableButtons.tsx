30feb4be7cda01d7b82be6d370f4666650bd95d5
jshin <arpegios@naver.com>
2023-09-18 03:56:08 +0900

    [EDIT] refactored expandableButtons.tsx

--- frontend/components/content/expandableButtons.tsx
+++ frontend/components/content/expandableButtons.tsx
@@ -3,112 +3,45 @@
 import React, { useState } from 'react';
 import styles from '../../styles/profile.module.css';
 
-const ExpandableButtons = () => {
-  const [activeButton1, setActiveButton1] = useState('최근 경기 기록');
-  const [activeButton2, setActiveButton2] = useState('게임 전적');
-  const [activeButton3, setActiveButton3] = useState('순위');
-  const [activeButton4, setActiveButton4] = useState('업적');
+const buttonData = [
+  { text: '최근 경기 기록', content: '최근 경기 기록 내용' },
+  { text: '게임 전적', content: '게임 전적 내용' },
+  { text: '순위', content: '순위 내용' },
+  { text: '업적', content: '업적 내용' },
+];
 
-  const toggleButton1 = (buttonText: any) => {
-    if (activeButton1 === buttonText) {
-      setActiveButton1('');
-    } else {
-      setActiveButton1(buttonText);
-    }
-  };
-  const toggleButton2 = (buttonText: any) => {
-    if (activeButton2 === buttonText) {
-      setActiveButton2('');
-    } else {
-      setActiveButton2(buttonText);
-    }
-  };
-  const toggleButton3 = (buttonText: any) => {
-    if (activeButton3 === buttonText) {
-      setActiveButton3('');
-    } else {
-      setActiveButton3(buttonText);
-    }
-  };
-  const toggleButton4 = (buttonText: any) => {
-    if (activeButton4 === buttonText) {
-      setActiveButton4('');
-    } else{
-      setActiveButton4(buttonText);
-    }
+const ExpandableButtons = () => {
+  const initialState = buttonData.map(obj => obj.text);
+  const [activeButtons, setActiveButtons] = useState(initialState);
+
+  const toggleButton = (index: number) => {
+    setActiveButtons((prevActiveButtons) => {
+      const newActiveButtons = [...prevActiveButtons];
+      newActiveButtons[index] = prevActiveButtons[index] === '' ? buttonData[index].text : '';
+      return newActiveButtons;
+    });
   };
 
   return (
     <div>
-      <div className={styles.buttonWrapper}>
-        <button
-          className={`centerItemBlock ${styles.infoBox}`}
-          onClick={() => toggleButton1('최근 경기 기록')}
-        >
-          최근 경기 기록
-        </button>
-        {activeButton1 === '최근 경기 기록' && (
-          <div className={styles.buttonContent}>
-            최근 경기 기록 내용
-          </div>
-        )}
-        {activeButton1 === '' && (<div className={styles.buttonContentFadeOut}>
-          최근 경기 기록 내용
-      </div>)}
-      </div>
-
-      <div className={styles.buttonWrapper}>
-        <button
-          className={`centerItemBlock ${styles.infoBox}`}
-          onClick={() => toggleButton2('게임 전적')}
-        >
-          게임 전적
-        </button>
-        {activeButton2 === '게임 전적' && (
-          <div className={styles.buttonContent}>
-            게임 전적 내용
-          </div>
-        )}
-        {activeButton2 === '' && (<div className={styles.buttonContentFadeOut}>
-          게임 전적 내용
-      </div>)}
-      </div>
-
-      <div className={styles.buttonWrapper}>
-        <button
-          className={`centerItemBlock ${styles.infoBox}`}
-          onClick={() => toggleButton3('순위')}
-        >
-          순위
-        </button>
-        {activeButton3 === '순위' && (
-          <div className={styles.buttonContent}>
-            순위 내용
-          </div>
-        )}
-        {activeButton3 === '' && (<div className={styles.buttonContentFadeOut}>
-        순위 내용
-      </div>)}
-      </div>
-
-      <div className={styles.buttonWrapper}>
-        <button
-          className={`centerItemBlock ${styles.infoBox}`}
-          onClick={() => toggleButton4('업적')}
-        >
-          업적
-        </button>
-        {activeButton4 === '업적' && (
-          <div className={styles.buttonContent}>
-            업적 내용
-          </div>
-        )}
-        {activeButton4 === '' && (<div className={styles.buttonContentFadeOut}>
-        업적 내용
-      </div>)}
-      </div>
+      {buttonData.map((button, index) => (
+        <div key={index} className={styles.buttonWrapper}>
+          <button
+            className={`centerItemBlock ${styles.infoBox}`}
+            onClick={() => toggleButton(index)}
+          >
+            {button.text}
+          </button>
+          {activeButtons[index] === button.text && (
+            <div className={styles.buttonContent}>{button.content}</div>
+          )}
+          {activeButtons[index] === '' && (
+            <div className={styles.buttonContentFadeOut}>{button.content}</div>
+          )}
+        </div>
+      ))}
     </div>
   );
-        }
+};
 
 export default ExpandableButtons;
