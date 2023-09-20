71fd986a75839763882f900e3d14f776794fa1b1
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-09-20 21:30:27 +0900

    edit game gateway

--- backend/chat-app/src/game/game.gateway.ts
+++ backend/chat-app/src/game/game.gateway.ts
@@ -17,6 +17,10 @@ export class GameGateway implements OnModuleInit, OnGatewayConnection, OnGateway
 		private userService: UserService,
 		) {}
 	
+	gameSocketMap = new Map<number, number>(); // userid, socketid
+	gameRoomMap = new Map<number, string>(); // userid, room
+	gameQueue = [];
+
 	@WebSocketServer() // 소켓인스턴스를 준다
 	server: Server;
 
@@ -27,72 +31,69 @@ export class GameGateway implements OnModuleInit, OnGatewayConnection, OnGateway
 		});
 	}
 
-	accessToken: any;
-	currentUser: User;
+	// accessToken: any;
+	// currentUser: User;
   
-	connectedUsers: any[] = [];
-	channelMembers: User[] = [];
-  
-	
-	private async definePlayer(client: Socket) {
-		try {
-			// this.accessToken = client.handshake.query.token;
-			this.accessToken = await this.authService.verifyToken(this.accessToken);
-			this.currentUser = null;
-			this.currentUser = await this.userService.getProfileByUserId(this.accessToken.id);
+	// private async definePlayer(client: Socket) {
+	// 	try {
+	// 		// this.accessToken = client.handshake.query.token;
+	// 		this.accessToken = await this.authService.verifyToken(this.accessToken);
+	// 		this.currentUser = null;
+	// 		this.currentUser = await this.userService.getProfileByUserId(this.accessToken.id);
 			
-			if (!this.currentUser) {
-			  return this.disconnect(client);
-			}
+	// 		if (!this.currentUser) {
+	// 		  return this.disconnect(client);
+	// 		}
 		
-		} catch (error) {
-			return this.disconnect(client);
-	  	}
-	}
+	// 	} catch (error) {
+	// 		return this.disconnect(client);
+	//   	}
+	// }
   
-	private disconnect(socket: Socket) {
-		socket.disconnect();
-	}
+	// private disconnect(socket: Socket) {
+	// 	socket.disconnect();
+	// }
   
-	private async getSocketId(id: number): Promise<Socket> {
-	  	for (let user of this.connectedUsers) {
-			let decoded = user.handshake.query.token;
-			decoded = await this.authService.verifyToken(decoded);
-			
-			if (decoded.id === id)
-				return user;
-	  		}
-  
-	  		return null;
-	}
-  
-  
-	async handleConnection(client: Socket) {
-		await this.definePlayer(client);
-  
-		if (this.currentUser) {
-			// client.data.currentUser = this.currentUser;
-			this.connectedUsers.push(client);
-		}
-	}
-  
-	handleDisconnect(client: any) {
-		this.connectedUsers = this.connectedUsers.filter(user => user.id !== client.id);
-	}
-	
-	@SubscribeMessage('movePlayer')
-	movePlayer(_: Socket, info: string) {
-    	console.log(_);
-		console.log(info);
-	}
 
-	@SubscribeMessage('newMessage')
-	onNewMessage(@MessageBody() body: any) {
-		console.log(body);
-		this.server.emit('onMessage', {
-			msg: 'New Message',
-			content: body,
-		});
-	}
+	// private async getSocketId(id: number): Promise<Socket> {
+	//   	// for (let user of this.connectedUsers) {
+	// 	// 	let decoded = user.handshake.query.token;
+	// 	// 	decoded = await this.authService.verifyToken(decoded);
+			
+	// 	// 	if (decoded.id === id)
+	// 	// 		return user;
+	//   	// 	}
+  
+	//   		return null;
+	// }
+  
+  
+	// async handleConnection(client: Socket) {
+	// 	await this.definePlayer(client);
+  
+	// 	if (this.currentUser) {
+	// 		// client.data.currentUser = this.currentUser;
+	// 		// this.connectedUsers.push(client);
+	// 	}
+	// }
+  
+	// handleDisconnect(client: any) {
+	// 	// this.connectedUsers = this.connectedUsers.filter(user => user.id !== client.id);
+	// }
+	
+	// @SubscribeMessage('gamequeue')
+	// movePlayer(_: Socket, info: string) {
+    // 	console.log(_);
+	// 	console.log(info);
+	// }
+
+	// @SubscribeMessage('newMessage')
+	// onNewMessage(@MessageBody() body: any) {
+	// 	console.log(body);
+	// 	this.server.emit('onMessage', {
+	// 		msg: 'New Message',
+	// 		content: body,
+	// 	});
+	// }
 
 }
