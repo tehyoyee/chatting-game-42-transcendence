ffe9bded11fa3586956f493027e31c925fa45925
지원 <jiwonk54723202@gmail.com>
2023-09-20 11:46:33 +0900

    [CHAT] post group message 완료

--- backend/chat-app/src/chat/dto/channel-dto.ts
+++ backend/chat-app/src/chat/dto/channel-dto.ts
@@ -1,22 +1,6 @@
 import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, MaxLength } from "class-validator";
 import { ChannelType } from "../enum/channel_type.enum";
 
-export class ChannelDto {
-    @IsNotEmpty()
-    @MaxLength(5)
-    name: string;
-
-    @IsNotEmpty()
-    type: string;
-
-    @IsOptional()
-    password?: string;
-
-    members: any[];
-
-}
-
-//--------------
 export class GroupChannelDto {
     @IsNotEmpty()
     @MaxLength(10)
