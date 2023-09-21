e6d496a7a31e3984e264d11638161394759cbcf0
Harim Seong <hseong@student.42seoul.kr>
2023-09-21 21:44:15 +0900

    FEAT: working on chat creation

--- frontend/components/content/chat_manage.tsx
+++ frontend/components/content/chat_manage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from '@/styles/chat_manage.module.css';
 import useSocketContext from '@/lib/socket';
 
@@ -8,33 +8,75 @@ enum Type {
 	Password,
 };
 
+const evt_create_normal = "create-group-channel";
+const evt_create_dm = "create-dm-channel";
+
 export function ChatMenu() {
-	const [type, setType] = useState<Type>(Type.Public);
-//	const { chatSocket } = useSocketContext();
+	const [chatType, setChatType] = useState<Type>(Type.Public);
+	const { chatSocket } = useSocketContext();
+
+	// NOTE: is it necessary to check socket established?
+	useEffect(() => {
+		if (!chatSocket) {
+		}
+	}, []);
 
 	function handleTypeChange(e: any) {
 		switch (Number(e.target.value)) {
 			case Type.Public:
-				setType(Type.Public);
+				setChatType(Type.Public);
 				break;
 			case Type.Private:
-				setType(Type.Private);
+				setChatType(Type.Private);
 				break;
 			case Type.Password:
-				setType(Type.Password);
+				setChatType(Type.Password);
 				break;
 		}
 	}
 
 	function handleCreation() {
+		const nameElem = document.querySelector('#chatNameField') as HTMLInputElement;
+		const passwordElem = document.querySelector('#chatPwd') as HTMLInputElement;
+		const name = nameElem.value;
+		const password = passwordElem?.value;
+
+		if (!chatSocket) {
+			console.error("chatsocket error");
+			return;
+		}
+		switch (chatType) {
+			case Type.Public:
+			case Type.Private:
+				chatSocket.emit(evt_create_normal, {
+					channelName: name,
+					channelType: chatType,
+				});
+				break;
+			case Type.Password:
+				chatSocket.emit(evt_create_normal, {
+					channelName: name,
+					channelType: chatType,
+					password: password,
+				});
+				break;
+		}
+		console.log(`${chatType}: chat creation request`);
+		if (!chatSocket) return;
+		chatSocket.on('join', () => {});
+		chatSocket.on('creation_fail', () => {});
+
+//		chatSocket.emit(evt_create_normal, formData);
 	}
 
 	return (
 		<div className={styles.chatMenu}>
-			<form onSubmit={(e) => {e.preventDefault(); handleCreation}}>
+			<form id='chatCreateForm' onSubmit={e => {e.preventDefault(); handleCreation();}}>
 				<label 
 					htmlFor="nameField">채팅방 이름</label>
 				<input 
+					id='chatNameField'
+					name='name'
 					style={{
 						margin: "6px",
 						border: "solid 1px black",
@@ -46,7 +88,7 @@ export function ChatMenu() {
 				<fieldset onChange={handleTypeChange}>
 					<legend>채팅방 종류</legend>
 					<div>
-						<input type='radio' id='public' name='type' value={Type.Public}/>
+						<input type='radio' id='public' name='type' value={Type.Public} defaultChecked/>
 						<label htmlFor='public'>Public</label>
 					</div>
 
@@ -61,28 +103,33 @@ export function ChatMenu() {
 					</div>
 				</fieldset>
 				{
-					(type == Type.Password) &&
-					<>
-						<label 
-							htmlFor="nameField">비밀번호</label>
-						<input 
-							style={{
-								margin: "6px",
-								border: "solid 1px black",
-								marginBottom: "16px", /* to align bottom button */
-							}}
-							type="text"
-							pattern="[a-zA-Z0-9]{4,16}"
-							required
-						/>
-					</> ||
-					<>
-					{/*to align bottom button*/}
-						<br />
-						<br />
-					</>
+					(chatType == Type.Password) &&
+						<>
+							<label 
+								htmlFor="nameField">비밀번호
+							</label>
+							<input 
+								id='chatPwd'
+								name='pwd'
+								style={{
+									margin: "6px",
+									border: "solid 1px black",
+									marginBottom: "16px", /* to align bottom button */
+								}}
+								type="text"
+								pattern="[a-zA-Z0-9]{4,16}"
+								required
+							/>
+						</> ||
+						<>
+						{/*to align bottom button*/}
+							<br />
+							<br />
+						</>
 				}
 				<button
+					id='chatCreateButton'
+					type='submit'
 					style={{
 						position: "relative",
 						top: "200px",
