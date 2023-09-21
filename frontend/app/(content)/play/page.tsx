d6b820e184bc04092a36f13718bb33db5bf435e6
Harim Seong <hseong@student.42seoul.kr>
2023-09-22 01:17:32 +0900

    FEAT: offline pingpong game on /game route

--- frontend/app/(content)/play/page.tsx
+++ frontend/app/(content)/play/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import Game from '@/components/content/game';
+// import styles from '@/styles/profile.module.css'
+// import d from '/frontend/public/default.png'
 import GamePlay from '@/components/content/game_play';
 
 export default function Page() {
   return (
   <>
-{/*  <Game></Game>  */}
-  <GamePlay></GamePlay>
+    <GamePlay></GamePlay>
   </>
   );
 }
