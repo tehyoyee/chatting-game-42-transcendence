eea4dbdd33c2bbcf6aa62c69b85ddebac7db8b61
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-09-24 22:10:48 +0900

    add streaming

--- backend/chat-app/src/user/user.controller.ts
+++ backend/chat-app/src/user/user.controller.ts
@@ -8,6 +8,10 @@ import { GameHistory } from 'src/game/game.history.entity';
 import { FileInterceptor } from '@nestjs/platform-express';
 // import { AuthGuard } from '@nestjs/passport';
 import * as config from 'config';
+import { StreamableFile } from '@nestjs/common';
+import { join } from 'path';
+import { createReadStream } from 'fs';
+import { HttpException } from '@nestjs/common';
 
 const g_debug = true;
 
@@ -32,10 +36,25 @@ export class UserController {
     // @UseGuards(AuthGuard())
     @Get('/profile/:id')
     async getProfileByUserId(@Param('id', ParseIntPipe) id: number): Promise<User> {
-				if (g_debug)
-					console.log('/profile/:id');
+		if (g_debug)
+			console.log('/profile/:id');
         return await this.userService.getProfileByUserId(id);
     }
+
+    @Get('/profile/avatar/:id')
+    async getFile(@Res({ passthrough: true }) res: Response, @Param('id') id ): Promise<StreamableFile> {
+        const found = await this.getProfileByUserId(id);
+        if (!found)
+            throw new HttpException('User not Found', 400);
+        const filePath = await this.userService.getAvatarByUserId(id);
+        const file = createReadStream(join(process.cwd(), ));
+        
+        res.headers.set('Content-Type', 'application/json');
+        res.headers.set('Content-Disposition', `attachment; filename=` + __dirname + `/../../uploads/${filePath}`);
+    
+        return new StreamableFile(file);
+    }
+ 
 
     // @UseGuards(AuthGuard())
     @Patch('/updateName/:id/:nickName')
@@ -52,7 +71,7 @@ export class UserController {
     async updateAvatar(@UploadedFile() file: Express.Multer.File, @Param('id', ParseIntPipe)id: number, @Res() res: Response) {
         const path = file.path.replace(__dirname + `/../../uploads`, '');
         await this.userService.updateAvatar(id, path + file.originalname);
-
+        
         return {
             fileName: file.originalname,
             savedPath: path.replace(/\\/gi, '/'),
