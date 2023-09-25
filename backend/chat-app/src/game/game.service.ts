048c8a1768fb829f97242454b380269ff6504fce
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-09-25 22:31:10 +0900

    gameHistory

--- backend/chat-app/src/game/game.service.ts
+++ backend/chat-app/src/game/game.service.ts
@@ -37,9 +37,9 @@ export class GameService {
 		}
 
 		// 게임기록 변경
-		let newGameHistory = await this.gameRepository.createGameHistory(winUser, winUser.user_id, loseUser.user_id, winUser.nickname, loseUser.nickname, point1, point2);
-		await this.userService.updateGameHistory(winUser.user_id, newGameHistory);
-		newGameHistory = await this.gameRepository.createGameHistory(loseUser, winUser.user_id, loseUser.user_id, winUser.nickname, loseUser.nickname, point2, point1);
-		await this.userService.updateGameHistory(winUser.user_id, newGameHistory);
+		const newGameHistory1 = await this.gameRepository.createGameHistory(winUser, winUser.user_id, loseUser.user_id, winUser.nickname, loseUser.nickname, point1, point2);
+		await this.userService.updateGameHistory(winUser.user_id, newGameHistory1);
+		const newGameHistory2 = await this.gameRepository.createGameHistory(loseUser, winUser.user_id, loseUser.user_id, winUser.nickname, loseUser.nickname, point2, point1);
+		await this.userService.updateGameHistory(loseUser.user_id, newGameHistory2);
 	}
 }
