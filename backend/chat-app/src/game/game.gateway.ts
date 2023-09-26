a82e61168f5a74c04d22b63b63ba7680635195de
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-09-27 00:34:47 +0900

    add inviteGame

--- backend/chat-app/src/game/game.gateway.ts
+++ backend/chat-app/src/game/game.gateway.ts
@@ -350,11 +350,11 @@ export class GameGateway implements OnModuleInit, OnGatewayConnection, OnGateway
 	async inviteGame(@ConnectedSocket() hostSocket: Socket, @MessageBody() invitation: any) {
 		const hostUser: User = await this.socketToUser(hostSocket);
 		const targetUserSocket: Socket = this.userSocketMap.get(invitation.targetUserId);
-		this.server.to(targetUserSocket.id).emit({
+		this.server.to(targetUserSocket.id).emit('getInvited', {
 			hostId: hostUser.user_id,
 			hostNickname: hostUser.nickname,
 			gameMode: invitation.gameMode
-		})
+		});
 	}
 
 	@SubscribeMessage('acceptGame')
