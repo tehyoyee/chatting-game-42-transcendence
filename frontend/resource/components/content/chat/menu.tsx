8ae33ffed23adac986200b9fd6d6775dcc2dcd0e
jshin <arpegios@naver.com>
2023-09-28 19:42:07 +0900

    [ADD] channel name in current channel

--- frontend/resource/components/content/chat/menu.tsx
+++ frontend/resource/components/content/chat/menu.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 
 import { Socket } from 'socket.io-client';
@@ -13,264 +15,243 @@ import ChatControl from './control';
 import usePlayerContext, { EPlayerState, TPlayerContext } from '@/components/content/player_state';
 import UserList from '@/components/structure/userList';
 import useAuthContext from '@/components/user/auth';
+import { channel } from 'diagnostics_channel';
 
 const serverUrl = `${process.env.NEXT_PUBLIC_APP_SERVER_URL}`
+const channelUrl = `${serverUrl}/chat/channel`;
 const chatInfoReqUrl = `${serverUrl}/chat/users-in-channel`;
 
 const fetcher = async (path: string): Promise<IChatMate[]> => {
-	const res = await fetch(path, {
-		method: 'GET',
-		credentials: 'include',
-	})
-	.then(res => {
-		if (!res.ok) throw new Error("invalid response");
-		return res.json()
-	});
-	return res;
+    const res = await fetch(path, {
+        method: 'GET',
+        credentials: 'include',
+    })
+    .then(res => {
+        if (!res.ok) throw new Error("invalid response");
+        return res.json()
+    });
+    return res;
 };
 
 export default function ChatMenu() {
-	const { user: userInfo } = useAuthContext();
-	const { chatSocket, gameSocket } = useSocketContext();
-	const chatContext = useChatContext();
-	const { user, setUser, joined, setJoined } = chatContext;
-	const [userList, updateUserList] = useFetch<IChatMate[]>(`${chatInfoReqUrl}/${userInfo.id}/${user.channel_id}`, [], fetcher);
-	const [controlModal, setControlModal] = useState<boolean>(false);
-	const playerContext = usePlayerContext();
-	const [isDm, setIsDm] = useState<boolean>(false);
+    const { user: userInfo } = useAuthContext();
+    const { chatSocket, gameSocket } = useSocketContext();
+    const chatContext = useChatContext();
+    const { user, setUser, joined, setJoined } = chatContext;
+    const [userList, updateUserList] = useFetch<IChatMate[]>(`${chatInfoReqUrl}/${userInfo.id}/${user.channel_id}`, [], fetcher);
+    const [controlModal, setControlModal] = useState<boolean>(false);
+    const playerContext = usePlayerContext();
+    const [channelName, setChannelName] = useState('');
 
-	useEffect(() => {
-		if (!chatSocket) return;
-		socketInit(chatSocket, chatContext, playerContext, updateUserList, setIsDm);
-		return () => {
-			socketOff(chatSocket);
-		};
-	}, [chatSocket, user]);
+    useEffect(() => {
+        if (!chatSocket) return;
+        socketInit(chatSocket, chatContext, playerContext, updateUserList);
+        
+        const getChannelName = async () => {
+              await fetch(`${channelUrl}/${user.channel_id}`, {
+                method: "GET",
+                credentials: "include",
+              })
+                .then((res) => res.json())
+                .then((data) => {
+                  setChannelName(data.channel_name);
+                })
+                .catch((err) => {
+                    console.log(`${channelUrl}: fetch failed: ${err}`);
+                });
+            };
+        getChannelName();
+    }, [chatSocket, user]);
 
-	useEffect(() => {
-		if (!chatSocket || !gameSocket) return;
-		chatSocket.on('refreshStatus', () => {
-			updateUserList();
-		});
-		gameSocket?.on('refreshGameStatus', () => {
-			updateUserList();
-		});
-		return () => {
-			chatSocket.off('refreshStatus');
-			gameSocket.off('refreshGameStatus');
-		}
-	}, []);
+    useEffect(() => {
+        if (!chatSocket || !gameSocket) return;
+        chatSocket.on('refreshStatus', () => {
+            updateUserList();
+        });
+        gameSocket?.on('refreshGameStatus', () => {
+            updateUserList();
+        });
+        return () => {
+            chatSocket.off('refreshStatus');
+            gameSocket.off('refreshGameStatus');
+        }
+    }, []);
 
-	return (
-		<SideBar
-			className={`${styles.chatList} full-background-color overflow-y-scroll overflow-x-hidden"`}>
-			<ul>
-				{!isDm &&
-					<div>
-						<UserList userList={userList} updateUserList={updateUserList}></UserList>
-					</div>
-				}
-				{
-					(user.user_type === EChatUserType.OWNER || user.user_type === EChatUserType.ADMIN) &&
-					<li>
-						<button
-							type='button'
-							onClick={(e) => 
-									{e.preventDefault(); setControlModal(true);}}
-							className={`${styles.chatRoomButton}`}
-							style={{
-								color: 'black',
-								backgroundColor: 'lightsalmon',
-							}}>
-							{'채널 설정'} 
-						</button>
-					</li>
-				}
-				{
-					controlModal && 
-						<Modal
-							onClose={setControlModal}
-							style={{}}
-							>
-							<ChatControl userList={userList}></ChatControl>
-						</Modal>
-				}
-				
-				<li>
-					<button
-						type='button'
-						onClick={(e) => 
-							{e.preventDefault(); chatSocket && exitChat(user, chatSocket);}}
-						className={`${styles.chatRoomButton}`}
-						style={{
-							color: 'black',
-							backgroundColor: 'lightsalmon',
-						}}>
-						{'채널 탈퇴'} 
-					</button>
-				</li>
-				<li>
-					<button
-						type='button'
-						onClick={(e) => 
-							{e.preventDefault(); chatSocket && closeChat(user, chatSocket);}}
-						className={`${styles.chatRoomButton}`}
-						style={{
-							color: 'black',
-							backgroundColor: 'lightcyan',
-						}}>
-						{'채널 닫기'} 
-					</button>
-				</li>
-				
-			</ul>
-		</SideBar>
-	);
+    return (
+        <SideBar
+            className={`${styles.chatList} full-background-color overflow-y-scroll overflow-x-hidden"`}>
+            <div style={{fontSize: '20px',}}>{channelName}</div>
+            <ul>
+                {
+                    <div>
+                        <UserList userList={userList} updateUserList={updateUserList}></UserList>
+                    </div>
+                }
+                {
+                    (user.user_type === EChatUserType.OWNER || user.user_type === EChatUserType.ADMIN) &&
+                    <li>
+                        <button
+                            type='button'
+                            onClick={(e) => 
+                                    {e.preventDefault(); setControlModal(true);}}
+                            className={`${styles.chatRoomButton}`}
+                            style={{
+                                color: 'black',
+                                backgroundColor: 'lightsalmon',
+                            }}>
+                            {'채널 설정'} 
+                        </button>
+                    </li>
+                }
+                {
+                    controlModal && 
+                        <Modal
+                            onClose={setControlModal}
+                            style={{}}
+                            >
+                            <ChatControl userList={userList}></ChatControl>
+                        </Modal>
+                }
+                
+                <li>
+                    <button
+                        type='button'
+                        onClick={(e) => 
+                            {e.preventDefault(); chatSocket && exitChat(user, chatSocket);}}
+                        className={`${styles.chatRoomButton}`}
+                        style={{
+                            color: 'black',
+                            backgroundColor: 'lightsalmon',
+                        }}>
+                        {'채널 탈퇴'} 
+                    </button>
+                </li>
+                <li>
+                    <button
+                        type='button'
+                        onClick={(e) => 
+                            {e.preventDefault(); chatSocket && closeChat(user, chatSocket);}}
+                        className={`${styles.chatRoomButton}`}
+                        style={{
+                            color: 'black',
+                            backgroundColor: 'lightcyan',
+                        }}>
+                        {'채널 닫기'} 
+                    </button>
+                </li>
+                
+            </ul>
+        </SideBar>
+    );
 }
 
 function exitChat(user: IChatUser, socket: Socket) {
-	if (!confirm("채널에서 탈퇴하시겠습니까?")) return;
-	console.log('exitChat request');
-	socket.emit('leave-channel', user.channel_id);
+    if (!confirm("채널에서 탈퇴하시겠습니까?")) return;
+    console.log('exitChat request');
+    socket.emit('leave-channel', user.channel_id);
 }
 
 function closeChat(user: IChatUser, socket: Socket) {
-	console.log('closeChat request');
-	socket.emit('close-channel-window', user.channel_id);
+    console.log('closeChat request');
+    socket.emit('close-channel-window', user.channel_id);
 }
 
 function socketOff(chatSocket: Socket) {
-	chatSocket.off('enter-dm-fail')
-	chatSocket.off('enter-dm-success')
-	chatSocket.off('leave-fail')
-	chatSocket.off('leave-success')
-	chatSocket.off('close-fail')
-	chatSocket.off('close-success')
-	chatSocket.off('got-kicked');
-	chatSocket.off('got-banned');
-	chatSocket.off('kick');
-	chatSocket.off('ban');
-	chatSocket.off('mute');
-	chatSocket.off('leave');
-	chatSocket.off('join');
-	chatSocket.off('admin');
+    chatSocket.off('leave-fail')
+    chatSocket.off('leave-success')
+    chatSocket.off('close-fail')
+    chatSocket.off('close-success')
+    chatSocket.off('got-kicked');
+    chatSocket.off('got-banned');
+    chatSocket.off('kick');
+    chatSocket.off('ban');
+    chatSocket.off('mute');
+    chatSocket.off('leave');
+    chatSocket.off('join');
 }
 
 function socketInit(
-	chatSocket: Socket,
-	chatContext: TChatContext,
-	playerContext: TPlayerContext,
-	updateUserList: Function,
-	setIsDm: React.Dispatch<React.SetStateAction<boolean>>,
+    chatSocket: Socket,
+    chatContext: TChatContext,
+    playerContext: TPlayerContext,
+    updateUserList: Function,
 ) {
-	const { user, setUser, joined, setJoined } = chatContext;
-	const { setPlayerState, setPlayerData } = playerContext;
+    const { user, setUser, joined, setJoined } = chatContext;
+    const { setPlayerState, setPlayerData } = playerContext;
 
-	function close() {
-		setJoined(false);
-		setUser({
-			...user,
-			channel_id: -1,
-		});
-		setPlayerData(null);
-	}
+    function close() {
+        setJoined(false);
+        setUser({
+            ...user,
+            channel_id: -1,
+        });
+        setPlayerData(null);
+    }
 
-	socketOff(chatSocket);
+    socketOff(chatSocket);
 
-	chatSocket.on('enter-dm-success', 
-		(data: IChatUser) => {
-			chatSocket.off('close-fail');
-			chatSocket.off('close-success');
-			chatSocket.on('close-fail', (msg) => {
-				alert('오류: DM을 보낼 수 없습니다.');
-				setUser(user);
-				console.log(`close-fail error: ${msg}`)
-			})
-			chatSocket.on('close-success', (msg) => {
-				console.log(`close-success: ${msg}`)
-				setUser(data);
-				setPlayerData(data);
-				setIsDm(true);
-			});
-			chatSocket.emit('close-channel-window', user.channel_id);
-			console.log('dm-enter-success: ', data);
-	});
-	chatSocket.off('enter-dm-fail', () => {
-		alert('오류: DM을 보낼 수 없습니다.');
-	});
+    chatSocket.on('leave-fail', (msg) => {console.log(`leave-fail error: ${msg}`)})
 
+    chatSocket.on('leave-success', (msg) => {
+        console.log(`leave-success: ${msg}`)
+        close();
+        socketOff(chatSocket);
+    });
 
-	chatSocket.on('enter-dm-fail', (msg) => {
-		console.log(`enter-dm fail: ${msg}`); 
-		alert('오류: DM을 보낼 수 없습니다.');
-	});
+    chatSocket.on('close-fail', (msg) => {console.log(`close-fail error: ${msg}`)})
 
-	chatSocket.on('leave-fail', (msg) => {console.log(`leave-fail error: ${msg}`)})
+    chatSocket.on('close-success', (msg) => {
+        console.log(`close-success: ${msg}`)
+        close();
+        socketOff(chatSocket);
+    });
 
-	chatSocket.on('leave-success', (msg) => {
-		console.log(`leave-success: ${msg}`)
-		close();
-		socketOff(chatSocket);
-	});
+    chatSocket.on('got-kicked', (msg) => {
+        console.log(`got-kicked: ${msg}`)
+        close();
+        alert('채널에서 퇴장당했습니다.');
+        socketOff(chatSocket);
+    });
 
-	chatSocket.on('close-fail', (msg) => {console.log(`close-fail error: ${msg}`)})
+    chatSocket.on('got-banned', (msg) => {
+        console.log(`got-banned: ${msg}`)
+        close();
+        alert('채널에서 영구 퇴장당했습니다.');
+        socketOff(chatSocket);
+    });
 
-	chatSocket.on('close-success', (msg) => {
-		console.log(`close-success: ${msg}`)
-		close();
-		socketOff(chatSocket);
-	});
+    /*
+    chatSocket.off('got-mutted');
+    chatSocket.on('got-mutted', (msg) => {
+        console.log(`got-mutted: ${msg}`)
+    });
+    */
 
-	chatSocket.on('got-kicked', (msg) => {
-		console.log(`got-kicked: ${msg}`)
-		close();
-		alert('채널에서 퇴장당했습니다.');
-		socketOff(chatSocket);
-	});
+    chatSocket.on('kick', () => {
+        console.log("an user got kicked");
+        updateUserList();
+    });
 
-	chatSocket.on('got-banned', (msg) => {
-		console.log(`got-banned: ${msg}`)
-		close();
-		alert('채널에서 영구 퇴장당했습니다.');
-		socketOff(chatSocket);
-	});
+    chatSocket.on('ban', () => {
+        console.log("an user got banned");
+        updateUserList();
+    });
 
-	/*
-	chatSocket.off('got-mutted');
-	chatSocket.on('got-mutted', (msg) => {
-		console.log(`got-mutted: ${msg}`)
-	});
-	*/
+    chatSocket.on('mute', () => {
+        console.log("an user got muted");
+        updateUserList();
+    });
 
-	chatSocket.on('admin', () => {
-		console.log("new admin");
-		updateUserList();
-	});
+    chatSocket.on('leave', (msg) => {
+        console.log("an user exited");
+        console.log(`exit: ${JSON.stringify(msg)}`)
+        updateUserList();
+    });
 
-	chatSocket.on('kick', () => {
-		console.log("an user got kicked");
-		updateUserList();
-	});
-
-	chatSocket.on('ban', () => {
-		console.log("an user got banned");
-		updateUserList();
-	});
-
-	chatSocket.on('mute', () => {
-		console.log("an user got muted");
-		updateUserList();
-	});
-
-	chatSocket.on('leave', (msg) => {
-		console.log("an user exited");
-		console.log(`exit: ${JSON.stringify(msg)}`)
-		updateUserList();
-	});
-
-	chatSocket.on('join', (msg) => {
-		console.log("new user joined");
-		console.log(`join: ${JSON.stringify(msg)}`)
-		updateUserList();
-	});
+    chatSocket.on('join', (msg) => {
+        console.log("new user joined");
+        console.log(`join: ${JSON.stringify(msg)}`)
+        updateUserList();
+    });
 }
+
